3f00b72f2a54ace64f7f4443ea64ce501c819b92
Tuan N. Truong <tuan.truong625@gmail.com>
2021-09-28 09:32:15 -0400

    Style Header

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,18 @@ import BaseHeader from './components/BaseHeader';
 
 function App() {
   return (
-    <div className="App">
-      <BaseHeader title="Spiltter" />
-      <label htmlFor="bill">
-        Bill
-        <input type="text" name="bill" placeholder="Enter bill..." />
-      </label>
+    <div style={{ backgroundColor: '#b9dee2' }}>
+      <div className="container mx-auto pt-5 h-screen flex flex-col items-center md:justify-center" style={{ backgroundColor: '#b9dee2' }}>
+        <BaseHeader title="splitter" />
 
-      
+        <div>
+          <label htmlFor="bill">
+            Bill
+            <input type="text" name="bill" placeholder="Enter bill..." />
+          </label>
+        </div>
+
+      </div>
     </div>
   );
 }
--- src/components/BaseHeader.tsx
+++ src/components/BaseHeader.tsx
@@ -1,7 +1,12 @@
 function BaseHeader({ title }: { title: string }): JSX.Element {
- return (
-   <h1 className="text-xl">{title}</h1>
- )
+  return (
+    <div className="py-6">
+      <h1 className="text-2xl tracking-widest uppercase flex flex-col font-medium">
+        <span className="pb-2">{title.slice(0, 4)}</span>
+        <span>{title.slice(4, 8)}</span>
+      </h1>
+    </div>
+  )
 }
 
 export default BaseHeader
